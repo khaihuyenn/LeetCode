0418ef1c26680bca5fe97da65f28ec3fd32981c0
Khai Huyen Nguyen <107144096+khaihuyenn@users.noreply.github.com>
2024-08-17 15:06:08 -0400

    Time: 27 ms (72.92%), Space: 14 MB (21.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def isPalindrome(self, s):
-        new_s = '' # Initialize an empty new string
-        for char in s:
-            if char.isalnum():
-                new_s += char.lower()
+        new_s = ''.join(char.lower() for char in s if char.isalnum())
         return new_s == new_s[::-1]
 
         
