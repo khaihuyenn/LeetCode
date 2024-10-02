0e7ff3292dbff9e314b26f527e6a04aa2d82cce8
Khai Huyen Nguyen <107144096+khaihuyenn@users.noreply.github.com>
2024-10-02 13:36:32 -0400

    Time: 15 ms (69.31%), Space: 11.5 MB (70.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,15 @@
 class Solution(object):
     def fib(self, n):
-        fib=[0,1]
-        for i in range(2, n + 1):
-            fib.append(fib[i - 1] + fib[i - 2])
-        return fib[n]
+        # Base case
+        if (n <= 1):
+            return n
+        curr = 0
+        prev1 = 1
+        prev2 = 0
+        for i in range(2, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return curr
+
         
