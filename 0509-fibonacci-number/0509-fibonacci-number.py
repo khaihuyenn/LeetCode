8f114dc72018e4655b5648385b6735c8bec7761f
Khai Huyen Nguyen <107144096+khaihuyenn@users.noreply.github.com>
2024-10-02 15:29:37 -0400

    Time: 5 ms (97.71%), Space: 11.4 MB (99.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def fib(self, n):
-        dp = [0] * (n + 1)
+        # Allocate dp table of size n + 1 all zeroes
+        dp = [0] * (n+1)
         # Base case
         if n == 0:
             return 0
         dp[1] = 1
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+            dp[i] = dp[i - 1] + dp[i -2]
         return dp[n]
-        
