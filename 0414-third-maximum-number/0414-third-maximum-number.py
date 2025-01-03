7b2f00698599234fca99d3dfc2dbcee4f87c7d05
Khai Huyen Nguyen <khaihuyen.nguyen5211@gmail.com>
2025-01-03 17:46:23 -0500

    Time: 3 ms (36.1%), Space: 13 MB (33.62%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,20 +1,16 @@
 class Solution(object):
     def thirdMax(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         first, second, third = float('-inf'), float('-inf'), float('-inf')
-        print(first, second, third)
+
         for num in nums:
             if num > first:
-                first, second, third = num, first, second
-            elif first > num > second:
-                second, third = num, second
-            elif second > num > third:
+                third = second
+                second = first
+                first  = num
+            elif second < num < first:
+                third = second
+                second = num
+            elif third < num < second:
                 third = num
-        if third != float('-inf'):
-            return third
-        else:
-            return first
+        return third if third != float('-inf') else first
         
