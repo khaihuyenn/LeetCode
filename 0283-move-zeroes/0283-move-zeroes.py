763bde210a11cc84ac0938f33c36b65b0ae5407d
Khai Huyen Nguyen <khaihuyen.nguyen5211@gmail.com>
2025-01-20 14:59:09 -0500

    Time: 5 ms (72.41%), Space: 13.3 MB (83.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        index = 0
+        i = 0
         for num in nums:
             if num != 0:
-                nums[index] = num
-                index += 1
-        while index < len(nums):
-            nums[index] = 0
-            index += 1
+                nums[i] = num
+                i += 1
+        while i < len(nums):
+            nums[i] = 0
+            i += 1
+        return nums
+        
