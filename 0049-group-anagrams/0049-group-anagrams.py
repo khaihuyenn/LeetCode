4905aad8f87366edfd948d0e2509f477177260be
Khai Huyen Nguyen <khaihuyen.nguyen5211@gmail.com>
2025-11-03 18:35:19 -0500

    Time: 68 ms (9.19%), Space: 16.9 MB (40.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        result = defaultdict(list)
-        for s in strs:
+        map = {}
+        for word in strs:
             counter = [0] * 26
-            for c in s:
-                counter[ord(c) - ord('a')] += 1
-            result[tuple(counter)].append(s)
-        return result.values()
-        
+            for char in word:
+                counter[ord(char) - ord('a')] += 1
+            key = ''.join(str(counter))
+            if key not in map:
+                map[key] = []
+            map[key].append(word)
+        return list(map.values())
